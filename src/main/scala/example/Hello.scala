e24127956180a266b98a298587adaf0a3f587c86
Janik Dotzel <janik.dotzel@lightbend.com>
2022-12-15 21:37:46 +0100

    feat: implement a hello world class with 100% test coverage

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,9 +1,14 @@
 package example
 
-object Hello extends Greeting with App {
-  println(greeting)
+object Hello extends App {
+  def helloOne(): Unit = println("hello one")
+  def helloTwo(): Unit = println("hello two")
+  def helloThree(): Unit = println("hello three")
+
+  helloOne()
+  helloTwo()
+  helloThree()
+
 }
 
-trait Greeting {
-  lazy val greeting: String = "hello"
-}
+
--- src/test/scala/example/HelloSpec.scala
+++ src/test/scala/example/HelloSpec.scala
@@ -4,7 +4,48 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class HelloSpec extends AnyFlatSpec with Matchers {
-  "The Hello object" should "say hello" in {
-    Hello.greeting shouldEqual "hello"
+
+  "The helloOne method" should "print hello one" in {
+    // Capture the standard output in a String
+    val output = new java.io.ByteArrayOutputStream()
+    Console.withOut(output) {
+      Hello.helloOne()
+    }
+    val printedOutput = output.toString
+
+    // Verify that the output contains the expected string
+    printedOutput should include("hello one")
+  }
+
+  "The helloTwo method" should "print hello two" in {
+    val output = new java.io.ByteArrayOutputStream()
+    Console.withOut(output) {
+      Hello.helloTwo()
+    }
+    val printedOutput = output.toString
+
+    printedOutput should include("hello two")
+  }
+
+  "The helloThree method" should "print hello three" in {
+    val output = new java.io.ByteArrayOutputStream()
+    Console.withOut(output) {
+      Hello.helloThree()
+    }
+    val printedOutput = output.toString
+
+    printedOutput should include("hello three")
+  }
+
+  "The hello object" should "print hello one, hello two, and hello three" in {
+    val output = new java.io.ByteArrayOutputStream()
+    Console.withOut(output) {
+      Hello.main(Array())
+    }
+    val printedOutput = output.toString
+
+    printedOutput should include("hello one")
+    printedOutput should include("hello two")
+    printedOutput should include("hello three")
   }
 }
