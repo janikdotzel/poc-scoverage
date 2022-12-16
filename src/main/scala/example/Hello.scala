15cb9304a595d8ee035870a6f551918d27965340
Janik Dotzel <janik.dotzel@lightbend.com>
2022-12-16 16:31:50 +0100

    test

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -4,6 +4,7 @@ object Hello extends App {
   def helloOne(): Unit = println("hello one")
   def helloTwo(): Unit = println("hello two")
   def helloThree(): Unit = println("hello three")
+  def helloFour(): Unit = println("hello four")
 
   helloOne()
   helloTwo()
